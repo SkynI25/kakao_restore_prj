0f877c4e1073e555d04f5a0c62368e29c1ec9061
Ki Tae Park <rlxo1651@daum.net>
2022-04-15 18:23:54 +0900

    text parsing setting

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Avatar from './images/avatar.jpg';
+import Axios from "axios";
 import './styles/Chat.scss';
+import mockTxt from "./utils/mock.txt";
 
 const Chat = () => {
+
+    const [authorTxt, setAuthorTxt] = useState("");
+
+    let modifyTxtFn = (param) => {
+      let userTxt = param.regExp(/\d{0,}년\s\d{0,}월\s\d{0,}일\s(오전|오후)\s\d{1,}:\d{1,},/i);
+    }
+
+    useEffect(() => {
+      Axios(mockTxt).then(res => {
+        setAuthorTxt(res.data);
+      });
+    }, []);
+
     return (
         <div className="Chat">
           <div className="header-wrapper">
@@ -34,7 +49,7 @@ const Chat = () => {
                   <img src={Avatar} />
                   <div className="message__content">
                     <span className="message__bubble">
-                      1
+                      {authorTxt}
                     </span>
                     <span className="message__author">Friend</span>
                   </div>
